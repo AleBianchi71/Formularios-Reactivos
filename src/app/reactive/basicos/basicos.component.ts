b301db2cb10b1da4c29884d2c98465ef884518e0
AleBianchi71 <bianchialejandro998@gmail.com>
2023-04-03 10:48:17 -0300

    validaciones de formulario

--- src/app/reactive/basicos/basicos.component.ts
+++ src/app/reactive/basicos/basicos.component.ts
@@ -1,30 +1,48 @@
-import { Component } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Component, OnInit } from "@angular/core";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
 
 @Component({
-  selector: 'app-basicos',
-  templateUrl: './basicos.component.html',
-  styles: [`
-  li{
-    cursor:pointer;
-  }`]
-  
+  selector: "app-basicos",
+  templateUrl: "./basicos.component.html",
+  styles: [
+    `
+      li {
+        cursor: pointer;
+      }
+    `,
+  ],
 })
-export class BasicosComponent {
- 
-//miFormulario : FormGroup = new FormGroup({
- // nombre     : new FormControl('RTX 4080ti'),
- // precio     : new FormControl(1500),
- // existencias: new FormControl(5),
-//})
+export class BasicosComponent implements OnInit{
 
-miFormulario : FormGroup = this.fb.group({
-  nombre     : ['RTX 4080ti', [Validators.required, Validators.minLength(3)]],
-  precio     : [0,[Validators.required, Validators.min(0)]],
-  existencias: [0,[Validators.required, Validators.min(0)]]
+  miFormulario: FormGroup = this.fb.group({
+    nombre: [, [Validators.required, Validators.minLength(3)]],
+    precio: [, [Validators.required, Validators.min(0)]],
+    existencias: [, [Validators.required, Validators.min(0)]],
+  });
 
-})
+  constructor(private fb: FormBuilder) {}
 
-constructor(private fb: FormBuilder){}
+  ngOnInit(){
+    this.miFormulario.reset({
+      nombre : 'RTX 4080ti',
+      precio : 1500
+    })
+  }
 
+  campoEsValido(campo: string) {
+    return (
+      this.miFormulario.controls[campo].errors && //evalue errores
+      this.miFormulario.controls[campo].touched
+    ); //evalua si fue tocado el campo
+  }
+
+  guardar() {
+    if (this.miFormulario.invalid) {
+      this.miFormulario.markAllAsTouched(); //muestra los errores al guardar si el campo esta vacio
+      return;
+    }
+
+    console.log(this.miFormulario.value);
+    this.miFormulario.reset(); //resetea el formulario una vez que se guardo
+  }
 }
